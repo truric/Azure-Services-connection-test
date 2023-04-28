25e9bb49c4d6b4a4c8e7971ef1cf34371d9f7930
rp1122334564 <ricardo.parada@publiqare.com>
2023-04-28 16:09:52 +0100

    fix: updated file path without folder

--- src/main/java/org/example/StorageAccount/AccessStorageAccount.java
+++ src/main/java/org/example/StorageAccount/AccessStorageAccount.java
@@ -8,13 +8,13 @@ import java.io.File;
 public class AccessStorageAccount {
     public static void main(String[] args) {
         String sasToken = PropertiesUtil.getProperty("azure.storage.account.sas-token");
-        String containerName = PropertiesUtil.getProperty("azure.storage.account.container-name");
         String blobName = PropertiesUtil.getProperty("azure.storage.account.blob");
         String storageEndpoint = PropertiesUtil.getProperty("azure.storage.account.endpoint");
 
-        String blobUrl = storageEndpoint + "/" + containerName + "/" + blobName + "?" + sasToken;
+        String blobUrl = storageEndpoint + "/" + blobName + "?" + sasToken;
 
         BlobClient blobClient = new BlobClientBuilder().endpoint(blobUrl).buildClient();
+
 
         File fileToUpload = new File("C:\\Users\\RP\\Desktop\\azureServicesConnection\\src\\main\\resources\\thumbnail.png");
         blobClient.uploadFromFile(fileToUpload.getAbsolutePath());
