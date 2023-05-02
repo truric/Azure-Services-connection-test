00478a6d5e333715b8a20441fffaf0cf093e483a
rp1122334564 <ricardo.parada@publiqare.com>
2023-05-02 14:49:31 +0100

    fix: filePath on AccessStorageAccount

--- src/main/java/org/example/StorageAccount/AccessStorageAccount.java
+++ src/main/java/org/example/StorageAccount/AccessStorageAccount.java
@@ -10,13 +10,14 @@ public class AccessStorageAccount {
         String sasToken = PropertiesUtil.getProperty("azure.storage.account.sas-token");
         String blobName = PropertiesUtil.getProperty("azure.storage.account.blob");
         String storageEndpoint = PropertiesUtil.getProperty("azure.storage.account.endpoint");
+        String filePath = PropertiesUtil.getProperty("azure.storage.file-path");
 
         String blobUrl = storageEndpoint + "/" + blobName + "?" + sasToken;
 
         BlobClient blobClient = new BlobClientBuilder().endpoint(blobUrl).buildClient();
 
 
-        File fileToUpload = new File("C:\\Users\\RP\\Desktop\\azureServicesConnection\\src\\main\\resources\\thumbnail.png");
+        File fileToUpload = new File(filePath);
         blobClient.uploadFromFile(fileToUpload.getAbsolutePath());
 
     }
